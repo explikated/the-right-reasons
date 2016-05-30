a7b10571a6134a06e0673878ca45afd75d83136b
Kate Dramstad <calculated.mayhem@gmail.com>
2016-05-29 18:03:01 -0700

    adding devise set up files

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,11 @@
 #
 
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
 
   has_many :league_users, :dependent => :destroy
   has_many :leagues, through: :league_users
